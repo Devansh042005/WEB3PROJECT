dc9799b5db47393ec01fb88ff3b5005e35324f81
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-04-30 09:25:06 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
+// https://eth-sepolia.g.alchemy.com/v2/biqcXdwSRcGo_OSJRMMg7tuN2wJHEZ5O
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/biqcXdwSRcGo_OSJRMMg7tuN2wJHEZ5O',
+      accounts: ['995cf700cfd8a77c5630126388f2287524efc2bbf3bf909cb8fdb0024f7b9bf6']
+    }
+  }
 };
