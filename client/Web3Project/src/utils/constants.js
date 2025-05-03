8f1f2c1887f33d4d700d951f44628d2489a66d4d
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-03 23:30:57 +0530

    Add files via upload

---
+++ client/Web3Project/src/utils/constants.js
@@ -0,0 +1,5 @@
+// import the abi after deployment
+// contract address aayega
+import abi from "./Transactions.json";
+export const contractAddress = "0x68F067239bfc9193fb1C4EE9f6d5710BB3EEb679";
+export const contractABI = abi.abi;
